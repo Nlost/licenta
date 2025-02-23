41473e9bb57887ea9553499325a2a7f22a38c558
Nlost <danbalanntl@gmail.com>
2025-02-23 21:48:35 +0200

    [OpenMV] Integrated SPI

--- openmv/main.py
+++ openmv/main.py
@@ -14,18 +14,24 @@ def main():
     cs = Pin("P3", Pin.IN, Pin.PULL_UP)
     led = LED("LED_BLUE")
     led1 = LED("LED_RED")
+    buf_8 = bytearray(1)
+    buf_16 = bytearray(2)
     while True:
         if cs.value() == 0:
             led.on()
-            time.sleep(500)
+            spi.recv(buf_8)
+            if buf_8 == "A":
+                spi.send(b'AB', timeout=300)
+                spi.send(b'BB', timeout=300)
+                spi.send(b'ZZ', timeout=300)
+                spi.send(b'SS', timeout=300)
+                spi.send(b'P', timeout=300)
             led.off()
-            time.sleep(500)
         else:
             # ML Continues #
             led1.on()
             time.sleep(1000)
             led1.off()
-            time.sleep(1000)
 
 # Run the main function
 main()
